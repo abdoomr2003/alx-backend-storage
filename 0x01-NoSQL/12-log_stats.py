c0b84e8bf8910b9edef76fadcf295fc7438d2f2f
abdoomr2003 <abdoomer1112003@gmail.com>
2024-06-19 00:50:34 +0300

    A

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -7,7 +7,7 @@ from pymongo import MongoClient
 
 
 if __name__ == "__main__":
-    client= MongoClient('mongodb://127.0.0.1:27017')
+    client = MongoClient('mongodb://127.0.0.1:27017')
     nginx_collection = client.logs.nginx
 
     stats = nginx_collection.count_documents({})
