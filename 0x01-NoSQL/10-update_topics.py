4fa689bd2bee9ab11130b222e8af5c7b185fd89b
abdoomr2003 <abdoomer1112003@gmail.com>
2024-06-18 13:36:26 +0300

    fix bug

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -22,5 +22,5 @@ def update_topics(mongo_collection, name, topics):
         pymongo.collection.Collection: The MongoDB collection after the
         update operation.
     """
-    mongo_collection.update_one({"name": name}, {"$set": {"topics": topics}})
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
     return mongo_collection
