27bc810271e08e0a8578a9f526591ed6bf8914b6
abdoomr2003 <abdoomer1112003@gmail.com>
2024-06-19 23:25:06 +0300

    hankasha

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,34 +1,23 @@
 #!/usr/bin/env python3
-""" expiring web cache module """
-
-import redis
+"""
+This module can be used as an expiring web cache and tracker
+"""
 import requests
-from typing import Callable
-from functools import wraps
-
-redis = redis.Redis()
+import redis
 
 
-def wrap_requests(fn: Callable) -> Callable:
-    """ Decorator wrapper """
-
-    @wraps(fn)
-    def wrapper(url):
-        """ Wrapper for decorator guy """
-        redis.incr(f"count:{url}")
-        cached_response = redis.get(f"cached:{url}")
-        if cached_response:
-            return cached_response.decode('utf-8')
-        result = fn(url)
-        redis.setex(f"cached:{url}", 10, result)
-        return result
-
-    return wrapper
-
-
-@wrap_requests
 def get_page(url: str) -> str:
-    """get page self descriptive
     """
+    Retrieve the HTML content of a URL and cache the result.
+    """
+    redis_client = redis.Redis()
+
+    count_key = f"count:{url}"
+
+    redis_client.incr(count_key)
+    redis_client.expire(count_key, 10)
+
     response = requests.get(url)
-    return response.text
+    html_content = response.text
+
+    return html_content
