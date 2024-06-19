0d4fa59b3496d2139fcaba98f3a5df3202835b8a
abdoomr2003 <abdoomer1112003@gmail.com>
2024-06-19 23:26:41 +0300

    hankasha

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,23 +1,34 @@
 #!/usr/bin/env python3
 """
-This module can be used as an expiring web cache and tracker
+Implements an expiring web cache and tracker
 """
-import requests
+from typing import Callable
+from functools import wraps
 import redis
+import requests
+redis_client = redis.Redis()
 
 
+def url_count(method: Callable) -> Callable:
+    """counts how many times an url is accessed"""
+    @wraps(method)
+    def wrapper(*args, **kwargs):
+        url = args[0]
+        redis_client.incr(f"count:{url}")
+        cached = redis_client.get(f'{url}')
+        if cached:
+            return cached.decode('utf-8')
+        redis_client.setex(f'{url}, 10, {method(url)}')
+        return method(*args, **kwargs)
+    return wrapper
+
+
+@url_count
 def get_page(url: str) -> str:
-    """
-    Retrieve the HTML content of a URL and cache the result.
-    """
-    redis_client = redis.Redis()
-
-    count_key = f"count:{url}"
-
-    redis_client.incr(count_key)
-    redis_client.expire(count_key, 10)
-
+    """get a page and cache value"""
     response = requests.get(url)
-    html_content = response.text
+    return response.text
 
-    return html_content
+
+if __name__ == "__main__":
+    get_page('http://slowwly.robertomurray.co.uk')
